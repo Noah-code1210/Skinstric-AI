0b9fb3f95a1c49545a36338a210d40bc6c55389f
Noah <noahjames1035@gmail.com>
2025-06-12 11:05:50 -0400

    Name & Location success message

--- src/pages/City.jsx
+++ src/pages/City.jsx
@@ -3,15 +3,16 @@ import NavIntro from "../components/NavIntro";
 import SpinningCircles from "../components/UI/SpinningCircles";
 import { Link, useNavigate } from "react-router-dom";
 import BackArrowButton from "../assets/LandingButton.png";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 AOS.init();
 
 function City() {
-  const [data, setData] = useState([]);
   const [showProceed, setShowProceed] = useState(false);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const [success, setSuccess] = useState(false);
+  const [showInput, setShowInput] = useState(true);
 
   function navInto() {
     navigate("/picture");
@@ -37,41 +38,53 @@ function City() {
                 </div>
               </div>
             ) : (
-              <div className="input__wrapper">
-                <h3 className="input__title">Click to type</h3>
-                <input
-                  type="text"
-                  placeholder="Your City Name"
-                  className="input__bar"
-                  onKeyDown={(event) => {
-                    if (event.key === "Enter") {
-                      setLoading(true);
-                      async function fetchLocation() {
-                        const response = await fetch(
-                          "https://us-central1-api-skinstric-ai.cloudfunctions.net/skinstricPhaseOne",
-                          {
-                            method: "POST",
-                            headers: {
-                              "Content-Type": "application/json",
-                            },
-                            body: JSON.stringify({
-                              name: "Your Name",
-                              location: "Your Location",
-                            }),
-                          }
-                        );
-                        const data = await response.json();
-                        console.log(data);
-                        setTimeout(() => {
-                          setLoading(false);
-                          setShowProceed(true);
-                        }, 3000);
+              <>
+                {success && (
+                  <div className="success__state">
+                    <h2 className="success__state--title">Thank you!</h2>
+                    <p className="success__state--para">
+                      Your name and location have been added
+                    </p>
+                  </div>
+                )}
+                { showInput && <div className="input__wrapper">
+                  <h3 className="input__title">Click to type</h3>
+                  <input
+                    type="text"
+                    placeholder="Your City Name"
+                    className="input__bar"
+                    onKeyDown={(event) => {
+                      if (event.key === "Enter") {
+                        setLoading(true);
+                        async function fetchLocation() {
+                          const response = await fetch(
+                            "https://us-central1-api-skinstric-ai.cloudfunctions.net/skinstricPhaseOne",
+                            {
+                              method: "POST",
+                              headers: {
+                                "Content-Type": "application/json",
+                              },
+                              body: JSON.stringify({
+                                name: "Your Name",
+                                location: "Your Location",
+                              }),
+                            }
+                          );
+                          const data = await response.json();
+                          console.log(data);
+                          setTimeout(() => {
+                            setLoading(false);
+                            setShowInput(false);
+                            setSuccess(true);
+                            setShowProceed(true);
+                          }, 3000);
+                        }
+                        fetchLocation();
                       }
-                      fetchLocation();
-                    }
-                  }}
-                />
-              </div>
+                    }}
+                  />
+                </div>}
+              </>
             )}
             <Link to="/introduction">
               <div className="back__btn--wrapper">
@@ -84,7 +97,7 @@ function City() {
               </div>
             </Link>
             {showProceed && (
-              <div className="proceed__btn--wrapper" onClick={navInto} >
+              <div className="proceed__btn--wrapper" onClick={navInto}>
                 <div className="proceed__arrow--title">Proceed</div>
                 <img
                   src={BackArrowButton}
